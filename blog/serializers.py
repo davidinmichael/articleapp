cb1544b092bf6b4cd1e83733dab5412ca3ec1c4d
davidinmichael <davidinmichael@gmail.com>
2023-07-13 10:01:48 +0100

    I debugged the ArticleSerializer and changed extra_keywords to extra_kwargs

--- blog/serializers.py
+++ blog/serializers.py
@@ -6,13 +6,13 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ['author', 'id', 'title', 'content', 'date_posted']
-        extra_keywords = {
+        extra_kwargs = {
             'author': {'read_only': True},
             'id': {'read_only': True}
         }
 
-        def update(self, instance, validated_data):
-            instance.title = validated_data.get('title', instance.title)
-            instance.content = validated_data.get('content', instance.content)
-            instance.save()
-            return instance
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.save()
+        return instance
