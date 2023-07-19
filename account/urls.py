c368957c62a942eed951aad456a871adec3eee1b
davidinmichael <davidinmichael@gmail.com>
2023-07-19 12:45:10 +0100

    I added the login functionality

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("register/", views.register_view, name="register"),
+    path("login/", obtain_auth_token, name="login"),
 ]
