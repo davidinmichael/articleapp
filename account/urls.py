e88371dda69aba70543eb4a6d1ff24ced1f3afb9
davidinmichael <davidinmichael@gmail.com>
2023-07-20 15:17:42 +0100

    I created the login view to use JWT

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
-    path("login/", obtain_auth_token, name="login"),
+    # path("login/", obtain_auth_token, name="login"),
+    path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.logout_view, name="logout"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,5 @@
-from rest_framework import permissions
+from rest_framework import permissions, status
 from django.contrib.auth.models import User
-
 from blog import serializers
 from .serializers import RegisterSerializer
 from rest_framework.decorators import api_view, permission_classes
@@ -31,6 +30,36 @@ class RegisterView(APIView):
                 data = serializer.errors
             return Response(data)
 
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+    def post(self, request):
+        if request.method == 'POST':
+            username = request.data.get('username')
+            password = request.data.get('password')
+
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return Response({'error': 'Username does not exist'})
+            
+            if user.check_password(password):
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': "Password Incorrect"})
+
+@api_view(['POST',])
+@permission_classes([permissions.IsAuthenticated])
+def logout_view(request):
+    if request.method == "POST":
+        request.user.auth_token.delete()
+        return Response({"message": "You are logged out, Login to continue"})
+    
+
+
 # Register Function Based View
 # @api_view(['POST',])
 # @permission_classes([permissions.AllowAny])
@@ -49,10 +78,3 @@ class RegisterView(APIView):
     #     else:
     #         data = serializer.errors
     # return Response(data)
-
-@api_view(['POST',])
-@permission_classes([permissions.IsAuthenticated])
-def logout_view(request):
-    if request.method == "POST":
-        request.user.auth_token.delete()
-        return Response({"message": "You are logged out, Login to continue"})
