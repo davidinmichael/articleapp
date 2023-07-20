8e75cf8a81610372abb72cacb6b04d3e097906da
davidinmichael <davidinmichael@gmail.com>
2023-07-20 14:46:15 +0100

    I added the functionality of JWT being generated automatically for new users

--- account/views.py
+++ account/views.py
@@ -8,22 +8,25 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Register Class Based View
 class RegisterView(APIView):
     permission_classes = [permissions.AllowAny]
-    
+
     def post(self, request):
         if request.method == "POST":
             serializer = RegisterSerializer(data=request.data)
             data = {}
             if serializer.is_valid():
                 user = serializer.save()
-                token = Token.objects.create(user=user).key
+                # token = Token.objects.create(user=user).key
+                refresh = RefreshToken.for_user(user)
                 data['response'] = "Account created successfully"
                 data['email'] = user.email
                 data['username'] = user.username
-                data['token'] = token
+                data['refresh'] = str(refresh)
+                data['access'] = str(refresh.access_token)
             else:
                 data = serializer.errors
             return Response(data)
