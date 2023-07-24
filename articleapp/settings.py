6ea051971d04c8aa91b94f243a8d0120a636f46c
davidinmichael <davidinmichael@gmail.com>
2023-07-24 15:39:18 +0100

    I changed the default search parameter

--- articleapp/settings.py
+++ articleapp/settings.py
@@ -131,6 +131,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'SEARCH_PARAM': 'query',
+    
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
