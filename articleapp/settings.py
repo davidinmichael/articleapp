273c328d4711c2f57307ff6debf117d03dd6ef5d
davidinmichael <davidinmichael@gmail.com>
2023-07-25 10:28:21 +0100

    I added global pagenumber pagination

--- articleapp/settings.py
+++ articleapp/settings.py
@@ -147,7 +147,9 @@ REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
         'user': '200/day'
-    }
+    },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6
 }
 
 SIMPLE_JWT = {
